18c2fba66077c9cf60687cc81d82183750782381
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-08-05 21:36:38 +0200

    chore: migrate to SDK 9.0, clean up unused code and update target framework

--- Program.fs
+++ Program.fs
@@ -1,5 +1,7 @@
-﻿open System
+open System
+open Setup
 
 [<EntryPoint>]
 let main argv = 
+    phases.systemInfo
     0
---
+++ Setup.fs
@@ -0,0 +1,15 @@
+namespace Setup
+
+open System
+
+
+     
+module Phases =
+    let userInfo = 
+        printfn "user: %s" Environment.UserName
+    
+    let
+        
+        
+
+
