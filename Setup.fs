fb973930327d1beed45f6ddf42af06da050e0db6
jcthekin1234 <93554396+jcthekin12345@users.noreply.github.com>
2025-08-05 16:17:26 +0200

    feat: phase one is setup for first time system boot

--- Setup.fs
+++ Setup.fs
@@ -3,12 +3,12 @@ namespace Setup
 open System
 
 
-module dialogue =
-     [<Literal>]
-     let introDialogue: string = "Hello..yOu ave switched oN the System."
      
 module phases =
-     let phaseOne =
+    [<Literal>]
+    let introDialogue: string = "Hello..yOu ave switched oN the System."
+    
+    let phaseOne =
         printfn "%s" dialogue.introDialogue
 
 
